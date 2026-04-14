e656b3d2d5b59eaaffb2ce177f9d746a9ae04f33
Abhay_Rawat <arawat1909@gmail.com>
2026-04-14 11:18:51 +0530

    Add files via upload

---
+++ VASP_job_submission/vasp6-C11-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -l nodes=4:ppn=24
+#PBS -q default
+#PBS -N C11-test
+#PBS -e test.err
+#PBS -o test.out
+echo PBS JOB id is $PBS_JOBID
+echo PBS_NODEFILE is $PBS_NODEFILE
+echo PBS_QUEUE is $PBS_QUEUE
+cat  $PBS_NODEFILE
+export I_MPI_FABRICS=shm:tmi
+cd $PBS_O_WORKDIR
+
+source /opt/parallel_studio_xe_2018/start.sh
+
+#module load vasp.6.1.2
+
+mpirun -hostfile $PBS_NODEFILE -np 96 /c11scratch/apps/vasp.6.1.2/bin/vasp_std | tee result
+
+
+
+
