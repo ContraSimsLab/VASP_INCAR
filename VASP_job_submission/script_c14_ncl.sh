fe407df39c611eb370848763e2eed614abcd4669
Abhay_Rawat <arawat1909@gmail.com>
2026-04-14 11:25:48 +0530

    Update and rename vasp6-C14-CPU-test.sh.txt to script_c14_ncl.sh

--- VASP_job_submission/script_c14_ncl.sh
+++ VASP_job_submission/script_c14_ncl.sh
@@ -28,4 +28,4 @@ echo Working directory is $PBS_O_WORKDIR
 NPROCS=`wc -l < $PBS_NODEFILE`
 NNODES=`uniq $PBS_NODEFILE | wc -l`
 
-mpirun -np 112 --machinefile $PBS_NODEFILE vasp_std | tee result-new
+mpirun -np 112 --machinefile $PBS_NODEFILE vasp_ncl | tee result-new
